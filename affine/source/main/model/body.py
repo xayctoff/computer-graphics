eff4f052766d5d19f3ddf649780c0606be2288e8
xayctoff <xayctoff31@gmail.com>
2021-01-31 12:08:24 +0700

    update model

--- affine/source/main/model/body.py
+++ affine/source/main/model/body.py
@@ -1,17 +1,48 @@
 import numpy as number
+import os
 
 from source.main.util.reader import read_from_file
 
 
 class Body:
     def __init__(self):
-        #   Считывание из файла списка координат точек объекта и списка рёбер, соединяющих эти точки
-        self.nodes = read_from_file('nodes.txt')
-        self.edges = read_from_file('edges.txt')
+        #   Считывание из файла списка координат точек объекта и списка рёбер, соединяющих данные точки
+        self._nodes = read_from_file(os.path.relpath("../source/resources/nodes.txt", os.path.dirname(__file__)))
+        self._edges = read_from_file(os.path.relpath("../source/resources/edges.txt", os.path.dirname(__file__)))
 
         #   Координаты объекта
-        self.coordinates = number.array([0, 0, 0, 1])
+        self._coordinates = number.array([0, 0, 0, 1])
         #   Значения углов осей объекта
-        self.angles = number.array([0, 0, 0, 0])
+        self._angles = number.array([0, 0, 0, 0])
         #   Размер объекта
-        self.size = number.array([1.0, 1.0, 1.0, 1.0])
+        self._size = number.array([1.0, 1.0, 1.0, 1.0])
+
+    def get_nodes(self):
+        return self._nodes
+
+    def set_nodes(self, nodes):
+        self._nodes = nodes
+
+    def get_edges(self):
+        return self._edges
+
+    def set_edges(self, edges):
+        self._edges = edges
+
+    def get_coordinates(self):
+        return self._coordinates
+
+    def set_coordinates(self, coordinates):
+        self._coordinates = coordinates
+
+    def get_angles(self):
+        return self._angles
+
+    def set_angles(self, angles):
+        self._angles = angles
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        self._size = size
--- affine/source/main/util/reader.py
+++ affine/source/main/util/reader.py
@@ -2,12 +2,12 @@ import numpy as number
 
 
 #   Чтение данных из файла
-#   @param name имя файла
+#   @param path путь к файлу
 #   @return список с выгруженными данными
-def read_from_file(name):
+def read_from_file(path):
     array = []
 
-    with open(name) as file:
+    with open(path) as file:
         for line in file:
             string = [int(i) for i in line.rstrip().split(' ')]
             array.append(string)
